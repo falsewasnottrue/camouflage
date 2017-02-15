3511673f726e7cc4b92514f960f935f95509d749
Rasmus Kuschel <rasmushofmann@gmail.com>
2017-02-15 10:45:35 +0100

    toc lesen

--- app/services/SponService.scala
+++ app/services/SponService.scala
@@ -3,17 +3,26 @@ package services
 import javax.inject.Inject
 
 import domain.ArticleInfo
+import org.jsoup.Jsoup
 import play.api.libs.ws.WSClient
+import scala.collection.JavaConversions._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class SponService @Inject() (ws: WSClient) {
 
-  def toc(): Future[Seq[ArticleInfo]] = {
+  def toc(): Future[Seq[ArticleInfo]] =
     ws.url("http://www.spiegel.de").get().map { response =>
-      println(response.body)
-      Nil
+      val jsoup = Jsoup.parse(response.body)
+      val teasers = jsoup.body().select("a[title]")
+      teasers.toList.map(teaser => {
+        val title = teaser.attr("title")
+        val link = teaser.attr("href")
+
+        val summary = "TODO"
+        println(s"TITLE $title")
+        ArticleInfo(title, summary, link)
+      })
     }
-  }
 }
